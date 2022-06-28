09ac9dad9e028912b5b8d6de548ca8aaa0d819f1
guillermosifu <sifuentes1201@gmail.com>
2022-06-27 21:10:52 -0500

    react

---
+++ src/Appejemplo.js
@@ -0,0 +1,38 @@
+import{useState} from "react";
+
+const Primercomponente = () =>{
+    //React tinene una funcion interna llamada useState
+    // el nombre de la funcion debe empezar con mayusula
+    //par poder usar useState debemos importarla desde react
+    //Al usar useState la forma en la cual creamos una variable cambia
+    //por que debemos defirni 2 cosas priemero el nombre de la variable y segundo 
+    //la funcion que se encargara de camabiar el estado 
+    //count : es la variable que usaremos para podermostrar el valor 
+    //setCount :funcion que se encarga de asiganrle el valor 
+
+const [count, setCount] = useState(0);
+
+//esta fucnion retornar una vista 
+// en react usamos return ()para indicar lo queesta dento de la parantesis
+
+const sumar = ()=> setCount(count +1);
+const restar =()=> setCount(count -1);
+
+return(
+
+ <div>
+<h1>hola mundo</h1>
+<h2>{count}</h2>
+<h3>hola grupo 8</h3>
+<button onClick= {sumar}>sumar</button>
+<button onClick={restar}>restar</button>
+
+ </div>
+   
+
+);
+
+
+};
+
+export default (Primercomponente);
---
+++ src/componentes/Footer.js
@@ -0,0 +1,16 @@
+import '../App.css';
+
+
+
+const Footer =()=>{
+    return(
+        <footer className='footer'>
+          <p>DERECHOS RESERVADOS</p> 
+           <span>guillermo</span>
+        </footer>
+    )
+}
+
+
+
+export default (Footer);
